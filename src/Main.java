cb69e686872b62abf4fbddb3cef0604b16fc6ba6
Filip Vastl <filip.vastl005@gmail.com>
2026-09-07 09:39:00 +0200

    funkcni volani metod a volitelnosti obtiznosti

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.concurrent.TimeUnit;
 
 public class Main {
     public static void main(String[] args){
@@ -6,22 +7,45 @@ public class Main {
         Scanner UserIn = new Scanner(System.in);
 
         System.out.println("Výtej hráči 1");
-        Thread.sleep(1000);
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
         System.out.println("Vyber si obtížnost");
         System.out.println("1 - Jednoduchá");
-        System.out.println("2 - Střední");
+        System.out.println("2 - Normální");
         System.out.println("3 - Složitá");
         int UserDiff = UserIn.nextInt();
 
         switch (UserDiff){
             case 1:
-                System.out.print("Dobrá volba srabe");
-                System.out.print("");
+                System.out.println("Dobrá volba srabe");
+                Game1();
+                break;
+            case 2:
+                System.out.println("Asi ok");
+                Game2();
+                break;
+            case 3:
+                System.out.println("Egoisto");
+                Game3();
+                break;
 
         }
     }
 
     static void Game1(){
+        System.out.println("Game1");
+    }
+
+    static void Game2(){
+        System.out.println("Game1");
+
+    }
+
+    static void Game3(){
+        System.out.println("Game1");
 
     }
 }
